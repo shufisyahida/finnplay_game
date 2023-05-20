45ca00be9cee416dec16ea7e2fda695175e067db
Shufi <shufisk@gmai.com>
2023-05-20 16:47:21 +0300

    search input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "./Input.module.css";
+import Image from "next/image";
 
 export default function Input({
   label,
   value,
   onChange,
+  search,
   ...inputProps
 }: {
   label: string;
+  search: boolean;
 } & React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
@@ -23,6 +26,15 @@ export default function Input({
       <label className={styles.label}>
         <div className={styles.text}>{label}</div>
       </label>
+      {search && (
+        <Image
+          width={16}
+          height={16}
+          alt="search icon"
+          src="/search.svg"
+          className={styles.search}
+        />
+      )}
     </div>
   );
 }
